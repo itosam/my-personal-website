844f8edb85f0bcdc7562683933478b2b66e3418c
Osamu Ito <mr.samito@gmail.com>
2022-08-20 23:52:06 -0400

    added javascript commands

--- script.js
+++ script.js
@@ -1,5 +1,2 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2);
 
-setTimeout(function(){alert("Hello! Welcome to my site!"); }, 3000);
+setTimeout(function(){alert("Hello! Welcome to my site!"); }, 10000);
